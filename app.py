beca31b0d6751cff0896cfeb10f2634e2085e7b8
nazar <nazar.khort@gmail.com>
2021-08-05 23:50:37 +0300

    Ввів клас Users, по якому буде створено таблицю. Назва класу буде назвою таблиці.

--- app.py
+++ app.py
@@ -7,5 +7,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db' # створив в 
 
 db = SQLAlchemy(app) # ввів екземпляр класу SQLAlchemy, через який якраз здійснюється робота з БД і якому передається посилання на поточну програму app.
 
+
+class Users(db.Model): # ввів клас Users, який наслідується від класу Model. Клас Model є базовим і якраз він перетворює класс Users в модель таблиці для SQLAlchemy.
+    # Поля таблиці прописуються як звичайні змінні, які посилаються на спеціальний клас Column. Цей клас якраз і вказує SQLAlchemy сприймати ці змінні як поля таблиці.
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True)
+    psw = db.Column(db.String(500), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self): # В кінці опису класу йде функція __repr__, яка визначає спосіб відображення класу в консолі. З її допомогою ми будемо виводити клас у вигляді рядка формату: <Users ідентифікатор>
+        return f"<users {self.id}>"
+
 if __name__ == "__main__":
     app.run(debug=True) # запуск веб-сервера
